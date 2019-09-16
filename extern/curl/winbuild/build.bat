a9e2e7595be710548908274b9fc71501bef7131d
Brian Fiete <bfiete@gmail.com>
2019-09-16 09:34:30 -0700

    CURL updates

--- extern/curl/winbuild/build.bat
+++ extern/curl/winbuild/build.bat
@@ -1,1 +1,1 @@
-nmake /f Makefile.vc mode=static VC=15 ENABLE_WINSSL=yes ENABLE_SSPI=yes MACHINE=x64 WITH_ZLIB=static WITH_DEVEL=../deps
+nmake /f Makefile.vc mode=static VC=15 RTLIBCFG=static ENABLE_WINSSL=yes ENABLE_SSPI=yes MACHINE=x64 WITH_ZLIB=static WITH_DEVEL=../deps
