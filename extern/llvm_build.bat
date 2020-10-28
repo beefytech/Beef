090f987c1a7fdcb4515598660f8fa91627ce8f42
Brian Fiete <bfiete@gmail.com>
2020-10-28 09:35:06 -0700

    LLVM 11 build fix

--- extern/llvm_build.bat
+++ extern/llvm_build.bat
@@ -3,20 +3,18 @@ PUSHD %~dp0
 @IF EXIST llvm-project_11_0_0 GOTO LLVM_HAS
 git clone --config core.autocrlf=false https://github.com/llvm/llvm-project.git llvm-project_11_0_0
 pushd llvm-project_11_0_0
-GOTO :LLVM_SET
 
-:LLVM_HAS
 pushd llvm-project_11_0_0
 git pull origin master
-
-:LLVM_SET
 git checkout llvmorg-11.0.0
 popd
-@IF EXIST llvm_win64_11_0_0 GOTO HAS_CONFIG
 
+:LLVM_HAS
+
+@IF EXIST llvm_win64_11_0_0 GOTO HAS_CONFIG
 mkdir llvm_win64_11_0_0
 cd llvm_win64_11_0_0
-cmake ../llvm-project_11_0_0/llvm -Thost=x64 -DLLVM_USE_CRT_DEBUG:STRING="MTd" -DLLVM_USE_CRT_RELEASE:STRING="MT"
+cmake ../llvm-project_11_0_0/llvm -G"Visual Studio 15 2017 Win64" -Thost=x64 -DLLVM_USE_CRT_DEBUG:STRING="MTd" -DLLVM_USE_CRT_RELEASE:STRING="MT"
 @GOTO DOBUILD
 
 :HAS_CONFIG
