9995803d60ddf25235ec606d8f03053a628d279c
Brian Fiete <bfiete@gmail.com>
2020-10-28 09:34:23 -0700

    Linux fixes for LLVM 11

--- extern/llvm_build.sh
+++ extern/llvm_build.sh
@@ -1,35 +1,35 @@
 #!/bin/bash
 set -e
 
-if [ ! -d llvm-project_8_0_0 ]; then
-	git clone https://github.com/llvm/llvm-project.git llvm-project_8_0_0
+if [ ! -d llvm-project_11_0_0 ]; then
+	git clone https://github.com/llvm/llvm-project.git llvm-project_11_0_0
 
-	if [ -d llvm-project_8_0_0 ]; then
-		cd llvm-project_8_0_0
- 		git checkout llvmorg-8.0.0
+	if [ -d llvm-project_11_0_0 ]; then
+		cd llvm-project_11_0_0
+ 		git checkout llvmorg-11.0.0
 		cd ..
 	fi 
 
 fi
 
-if [ ! -d llvm_linux_8_0_0 ]; then
-	mkdir llvm_linux_8_0_0	
+if [ ! -d llvm_linux_11_0_0 ]; then
+	mkdir llvm_linux_11_0_0	
 fi
 
-if [ ! -d llvm_linux_8_0_0/bin ]; then
-	cd llvm_linux_8_0_0
-	cmake ../llvm-project_8_0_0/llvm
+if [ ! -d llvm_linux_11_0_0/bin ]; then
+	cd llvm_linux_11_0_0
+	cmake ../llvm-project_11_0_0/llvm
 	cmake --build .
 	cd ..
 fi
 
-if [ ! -d llvm_linux_rel_8_0_0 ]; then
-	mkdir llvm_linux_rel_8_0_0	
+if [ ! -d llvm_linux_rel_11_0_0 ]; then
+	mkdir llvm_linux_rel_11_0_0	
 fi
 
-if [ ! -d llvm_linux_rel_8_0_0/bin ]; then
-	cd llvm_linux_rel_8_0_0
-	cmake ../llvm-project_8_0_0/llvm -DCMAKE_BUILD_TYPE:String=Release
+if [ ! -d llvm_linux_rel_11_0_0/bin ]; then
+	cd llvm_linux_rel_11_0_0
+	cmake ../llvm-project_11_0_0/llvm -DCMAKE_BUILD_TYPE:String=Release
 	cmake --build .
 	cd ..
 fi
@@ -38,4 +38,4 @@ if [ ! -d ../IDE/dist/llvm/bin ]; then
 	mkdir ../IDE/dist/llvm
 	mkdir ../IDE/dist/llvm/bin
 fi
-cp llvm_linux_rel_8_0_0/bin/llvm-ar ../IDE/dist/llvm/bin
+cp llvm_linux_rel_11_0_0/bin/llvm-ar ../IDE/dist/llvm/bin
