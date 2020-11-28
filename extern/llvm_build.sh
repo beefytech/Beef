71a0f70d868ad3e86ca9bded0b41969ae16d06b3
Green Sky <green@g-s.xyz>
2020-11-28 23:23:27 +0100

    llvm: only build "needed" parts, extracted currently needed parts to llvm_targets.txt

--- extern/llvm_build.sh
+++ extern/llvm_build.sh
@@ -2,39 +2,40 @@
 set -e
 
 if [ ! -d llvm-project_11_0_0 ]; then
-  if [ -f llvm-11.0.0.src.tar.xz ]; then # if user downloaded llvm-11.0.0.src.tar.xz then use it instead
-	tar -xf llvm-11.0.0.src.tar.xz
-	mkdir llvm-project_11_0_0
-	mv llvm-11.0.0.src llvm-project_11_0_0/llvm
-  else # shallow git clone llvm repo if llvm-11.0.0.src.tar.xz does not exists 
-	git clone --depth 1 --branch llvmorg-11.0.0 https://github.com/llvm/llvm-project.git llvm-project_11_0_0
-  fi
+	if [ -f llvm-11.0.0.src.tar.xz ]; then # if user downloaded llvm-11.0.0.src.tar.xz then use it instead
+		tar -xf llvm-11.0.0.src.tar.xz
+		mkdir llvm-project_11_0_0
+		mv llvm-11.0.0.src llvm-project_11_0_0/llvm
+	else # shallow git clone llvm repo if llvm-11.0.0.src.tar.xz does not exists
+		git clone --depth 1 --branch llvmorg-11.0.0 https://github.com/llvm/llvm-project.git llvm-project_11_0_0
+	fi
 fi #end if llvm-project_11_0_0 exists
 
 if [ ! -d llvm_linux_11_0_0 ]; then
-	mkdir llvm_linux_11_0_0	
+	mkdir llvm_linux_11_0_0
 fi
 
 if [ ! -d llvm_linux_11_0_0/bin ]; then
 	cd llvm_linux_11_0_0
-	cmake ../llvm-project_11_0_0/llvm
-	cmake --build .
+	cmake ../llvm-project_11_0_0/llvm -DLLVM_TARGETS_TO_BUILD="AArch64;ARM;X86;WebAssembly" -DCMAKE_BUILD_TYPE:String="Debug"
+	cmake --build . -t $(cat ../llvm_targets.txt)
 	cd ..
 fi
 
 if [ ! -d llvm_linux_rel_11_0_0 ]; then
-	mkdir llvm_linux_rel_11_0_0	
+	mkdir llvm_linux_rel_11_0_0
 fi
 
 if [ ! -d llvm_linux_rel_11_0_0/bin ]; then
 	cd llvm_linux_rel_11_0_0
-	cmake ../llvm-project_11_0_0/llvm -DCMAKE_BUILD_TYPE:String=Release
-	cmake --build .
+	cmake ../llvm-project_11_0_0/llvm -DLLVM_TARGETS_TO_BUILD="AArch64;ARM;X86;WebAssembly" -DCMAKE_BUILD_TYPE:String="Release"
+	cmake --build . -t $(cat ../llvm_targets.txt)
 	cd ..
 fi
-	
+
 if [ ! -d ../IDE/dist/llvm/bin ]; then
 	mkdir ../IDE/dist/llvm
 	mkdir ../IDE/dist/llvm/bin
 fi
 cp llvm_linux_rel_11_0_0/bin/llvm-ar ../IDE/dist/llvm/bin
+
