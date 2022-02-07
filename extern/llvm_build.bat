114a90dd5e9b7b17bd954f111d71f218cb2cd791
Brian Fiete <bfiete@gmail.com>
2022-02-07 14:23:47 -0500

    llvm build script updates, vcxproj llvm path updates

--- extern/llvm_build.bat
+++ extern/llvm_build.bat
@@ -1,25 +1,29 @@
 PUSHD %~dp0
 
-@IF EXIST llvm-project_11_0_0 GOTO LLVM_HAS
-git clone --depth 1 --branch llvmorg-11.0.0 --config core.autocrlf=false https://github.com/llvm/llvm-project.git llvm-project_11_0_0
-@rem pushd llvm-project_11_0_0
+@IF EXIST llvm-project_13_0_1 GOTO LLVM_HAS
+git clone --depth 1 --branch llvmorg-13.0.1 --config core.autocrlf=false https://github.com/llvm/llvm-project.git llvm-project_13_0_1
+@IF %ERRORLEVEL% NEQ 0 GOTO HADERROR
 
 :LLVM_HAS
 
-@IF EXIST llvm_win64_11_0_0 GOTO HAS_CONFIG
-mkdir llvm_win64_11_0_0
-cd llvm_win64_11_0_0
-cmake ../llvm-project_11_0_0/llvm -G"Visual Studio 16 2019" -Ax64 -Thost=x64 -DLLVM_USE_CRT_DEBUG:STRING="MTd" -DLLVM_USE_CRT_RELEASE:STRING="MT" -DLLVM_TARGETS_TO_BUILD="AArch64;ARM;X86;WebAssembly"
+@IF EXIST llvm_win64_13_0_1 GOTO HAS_CONFIG
+mkdir llvm_win64_13_0_1
+cd llvm_win64_13_0_1
+cmake ../llvm-project_13_0_1/llvm -G"Visual Studio 16 2019" -Ax64 -Thost=x64 -DLLVM_USE_CRT_DEBUG:STRING="MTd" -DLLVM_USE_CRT_RELEASE:STRING="MT" -DLLVM_TARGETS_TO_BUILD="AArch64;ARM;X86;WebAssembly"
+@IF %ERRORLEVEL% NEQ 0 GOTO HADERROR
 @GOTO DOBUILD
 
 :HAS_CONFIG
-cd llvm_win64_11_0_0
+cd llvm_win64_13_0_1
 @GOTO DOBUILD
 
 :DOBUILD
 set /p LLVM_TARGETS=<../llvm_targets.txt
 cmake --build . -t %LLVM_TARGETS% --config Debug
+@IF %ERRORLEVEL% NEQ 0 GOTO HADERROR
 cmake --build . -t %LLVM_TARGETS% --config Release
+@IF %ERRORLEVEL% NEQ 0 GOTO HADERROR
+echo done > _Done.txt
 
 :SUCCESS
 @ECHO SUCCESS!
