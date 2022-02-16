db065d0cd784e13cc099b8dee42db5d1c884ee5e
Brian Fiete <bfiete@gmail.com>
2022-02-16 16:46:08 -0500

    wasm batch fixes

--- bin/build_wasm.bat
+++ bin/build_wasm.bat
@@ -1,10 +1,14 @@
 PUSHD %~dp0..\
 
+SETLOCAL
+
 mkdir wasm
 cd wasm
 @IF %ERRORLEVEL% NEQ 0 GOTO HADERROR
 
+set LIBPATH=..\bin
 IF NOT EXIST ..\BeefRT\rt\Chars.cpp GOTO SKIPCOPY
+set LIBPATH=..\IDE\dist
 mkdir src
 mkdir src\rt
 copy ..\BeefRT\rt\* src\rt\
@@ -30,7 +34,7 @@ IF "%1" EQU "setup" GOTO SUCCESS
 
 call emcc src\rt\Chars.cpp src\rt\Math.cpp src\rt\Object.cpp src\rt\Thread.cpp src\rt\Internal.cpp src\BeefySysLib\platform\wasm\WasmCommon.cpp src\BeefySysLib\Common.cpp src\BeefySysLib\util\String.cpp src\BeefySysLib\util\UTF8.cpp src\BeefySysLib\third_party\utf8proc\utf8proc.c -Isrc\ -Isrc\BeefySysLib -Isrc\BeefySysLib\platform\wasm -g -DBF_DISABLE_FFI -c -s WASM=1 -s USE_PTHREADS=1
 @IF %ERRORLEVEL% NEQ 0 GOTO HADERROR
-call emar r ..\IDE\dist\Beef042RT32_wasm.a Common.o Internal.o Chars.o Math.o Object.o String.o Thread.o UTF8.o utf8proc.o WasmCommon.o
+call emar r %LIBPATH%\Beef042RT32_wasm.a Common.o Internal.o Chars.o Math.o Object.o String.o Thread.o UTF8.o utf8proc.o WasmCommon.o
 @IF %ERRORLEVEL% NEQ 0 GOTO HADERROR
 
 :SUCCESS
