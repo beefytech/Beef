84e5a8818314412be39143ebdda54f96020dd24f
Brian Fiete <bfiete@gmail.com>
2022-03-31 06:57:43 -0700

    cloc update

--- bin/cloc_all.bat
+++ bin/cloc_all.bat
@@ -1,16 +1,16 @@
-\bin\cloc ..\Beefy2D --force-lang="C#",bf
+\bin\cloc ..\Beefy2D --exclude-dir=recovery --force-lang="C#",bf
 \bin\cloc ..\BeefRT --exclude-dir=gperftools --force-lang="C#",bf
 \bin\cloc ..\BeefySysLib --exclude-dir=third_party --force-lang="C#",bf
-\bin\cloc ..\IDE --exclude-dir=corlib,mintest,dist,Tests --force-lang="C#",bf
-\bin\cloc ..\IDE\Tests --force-lang="C#",bf
+\bin\cloc ..\IDE --exclude-dir=corlib,mintest,dist,Tests,recovery --force-lang="C#",bf
+\bin\cloc ..\IDE\Tests --exclude-dir=recovery,Rando --force-lang="C#",bf
 \bin\cloc ..\IDEHelper --exclude-dir=Compiler,Backend,Linker,Tests --force-lang="C#",bf
 \bin\cloc ..\IDEHelper\Compiler --force-lang="C#",bf
 \bin\cloc ..\IDEHelper\Backend --force-lang="C#",bf
 \bin\cloc ..\IDEHelper\Linker --force-lang="C#",bf
-\bin\cloc ..\IDEHelper\Tests --exclude-dir=Rando --force-lang="C#",bf
+\bin\cloc ..\IDEHelper\Tests --exclude-dir=Rando,Beef.Linq,BeefLinq,recovery --force-lang="C#",bf
 \bin\cloc ..\IDE\BeefBoot --force-lang="C#",bf
 \bin\cloc ..\IDE\BeefBuild --force-lang="C#",bf
-\bin\cloc ..\BeefLibs --force-lang="C#",bf
-\bin\cloc ..\BeefTools --exclude-dir=gperftools --force-lang="C#",bf
-\bin\cloc ..\..\Beef_website\BeefTools --exclude-dir=gperftools,install --force-lang="C#",bf
-\bin\cloc ..\..\Beef_website\Samples --force-lang="C#",bf
+\bin\cloc ..\BeefLibs --exclude-dir=recovery --force-lang="C#",bf
+\bin\cloc ..\BeefTools --exclude-dir=gperftools,recovery --force-lang="C#",bf
+\bin\cloc ..\..\Beef_website\BeefTools --exclude-dir=gperftools,install,recovery --force-lang="C#",bf
+\bin\cloc ..\..\Beef_website\Samples --exclude-dir=recovery --force-lang="C#",bf
