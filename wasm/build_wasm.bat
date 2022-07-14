e66c4900afb2d8d0f4f28ca8a05d631c43fb990f
disarray2077 <86157825+disarray2077@users.noreply.github.com>
2022-07-14 13:05:28 -0300

    Fix build_wasm.bat

--- wasm/build_wasm.bat
+++ wasm/build_wasm.bat
@@ -26,13 +26,15 @@ mkdir src\BeefySysLib\third_party\utf8proc
 copy ..\BeefySysLib\third_party\utf8proc\* src\BeefySysLib\third_party\utf8proc
 mkdir src\BeefySysLib\third_party\stb
 copy ..\BeefySysLib\third_party\stb\* src\BeefySysLib\third_party\stb
+mkdir src\BeefySysLib\third_party\putty
+copy ..\BeefySysLib\third_party\putty\* src\BeefySysLib\third_party\putty
 :SKIPCOPY
 
 IF "%1" EQU "setup" GOTO SUCCESS
 
-call emcc src\rt\Chars.cpp src\rt\Math.cpp src\rt\Object.cpp src\rt\Thread.cpp src\rt\Internal.cpp src\BeefySysLib\platform\wasm\WasmCommon.cpp src\BeefySysLib\Common.cpp src\BeefySysLib\util\String.cpp src\BeefySysLib\util\UTF8.cpp src\BeefySysLib\third_party\utf8proc\utf8proc.c -Isrc\ -Isrc\BeefySysLib -Isrc\BeefySysLib\platform\wasm -g -DBF_DISABLE_FFI -c -s WASM=1 -s USE_PTHREADS=1
+call emcc src\rt\Chars.cpp src\rt\Math.cpp src\rt\Object.cpp src\rt\Thread.cpp src\rt\Internal.cpp src\BeefySysLib\platform\wasm\WasmCommon.cpp src\BeefySysLib\Common.cpp src\BeefySysLib\util\String.cpp src\BeefySysLib\util\UTF8.cpp src\BeefySysLib\third_party\utf8proc\utf8proc.c src\BeefySysLib\third_party\putty\wildcard.c -Isrc\ -Isrc\BeefySysLib -Isrc\BeefySysLib\platform\wasm -g -DBF_DISABLE_FFI -c -s WASM=1 -s USE_PTHREADS=1
 @IF %ERRORLEVEL% NEQ 0 GOTO HADERROR
-call emar r %LIBPATH%\Beef042RT32_wasm.a Common.o Internal.o Chars.o Math.o Object.o String.o Thread.o UTF8.o utf8proc.o WasmCommon.o
+call emar r %LIBPATH%\Beef042RT32_wasm.a Common.o Internal.o Chars.o Math.o Object.o String.o Thread.o UTF8.o utf8proc.o wildcard.o WasmCommon.o
 @IF %ERRORLEVEL% NEQ 0 GOTO HADERROR
 
 :SUCCESS
