6d386d163804d88dec773899dd34eccdaae84da9
flying-dude <dude@flyspace.dev>
2022-08-15 15:59:30 +0200

    guard against redefinition compile errors

--- BeefySysLib/Common.h
+++ BeefySysLib/Common.h
@@ -1,7 +1,12 @@
 #pragma once
 
+#ifndef __STDC_LIMIT_MACROS
 #define __STDC_LIMIT_MACROS
+#endif
+
+#ifndef __STDC_CONSTANT_MACROS
 #define __STDC_CONSTANT_MACROS
+#endif
 
 // #include <string>
 // #include <map>
