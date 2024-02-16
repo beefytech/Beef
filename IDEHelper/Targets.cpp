ec2d888470510b3252bdc22f55f3ca5572a95b2f
disarray2077 <86157825+disarray2077@users.noreply.github.com>
2024-02-16 18:40:58 -0300

    Use pre-built LLVM if available

--- IDEHelper/Targets.cpp
+++ IDEHelper/Targets.cpp
@@ -6,11 +6,15 @@ USING_NS_BF;
 
 BF_EXPORT void BF_CALLTYPE Targets_Create()
 {
+#ifndef __linux__
 	gX86Target = new X86Target();
+#endif
 }
 
 BF_EXPORT void BF_CALLTYPE Targets_Delete()
 {
+#ifndef __linux__
 	delete gX86Target;
 	gX86Target = NULL;
+#endif
 }
